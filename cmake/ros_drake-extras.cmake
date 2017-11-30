b1d996c5c575b66d232701688fb09c716aa08a46
Jose Luis Rivero <jrivero@osrfoundation.org>
2017-11-30 17:41:03 +0100

    Improve the error message

--- cmake/ros_drake-extras.cmake
+++ cmake/ros_drake-extras.cmake
@@ -1,10 +1,27 @@
 # Depend on system install of Gazebo
+  
 if (NOT drake_DIR)
   set(drake_DIR "/opt/drake")
 endif()
 
-find_package(drake REQUIRED
-    PATHS ${drake_DIR})
+# Keep a copy of original drake_DIR. It is overwritten by
+# the find_package call
+set(drake_string_DIR "${drake_DIR}")
+
+find_package(drake QUIET PATHS ${drake_DIR})
+
+if (NOT drake_FOUND)
+  message(FATAL_ERROR "\
+
+ =====================================================================\n\
+ DRAKE INSTALLATION NOT FOUND\n\
+ Drake installation was not found in ${drake_string_DIR}.\n\
+ -Ddrake_DIR= can be used to pass the root of an existing drake setup.\n\
+ Please check http://drake.mit.edu/installation.html for new\n\
+ installations.\n\
+ =====================================================================\n\
+ ")
+endif()
 
 message(STATUS "DRAKE installation found at: ${drake_DIR}")
 
